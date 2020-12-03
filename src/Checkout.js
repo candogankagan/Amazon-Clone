d8cad8b943bcd70f3c7b5aaf5340e9f3e4c7bf97
Kagan Candogan <candogankagan@gmail.com>
2020-12-03 15:47:49 +0300

    checkout products made

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import './Checkout.css';
-import Subtotal from './Subtotal';
+import React from 'react'
+import './Checkout.css'
+import { useStateValue } from './StateProvider'
+import Subtotal from './Subtotal'
+import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue()
   return (
-    <div className="checkout">
-      <div className="checkout_left">
+    <div className='checkout'>
+      <div className='checkout_left'>
         <img
-          className="checkout_add"
-          src="https://www.webfx.com/amazon/img/types-of-amazon-ads__03.png"
-          alt=""
+          className='checkout_add'
+          src='https://www.webfx.com/amazon/img/types-of-amazon-ads__03.png'
+          alt=''
         />
         <div>
-          <h2 className="checkout_title">Your Shopping Basket</h2>
+          <h2 className='checkout_title'>Your Shopping Basket</h2>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
-
-
-        
       </div>
 
-      <div className="checkout_right">
+      <div className='checkout_right'>
         <Subtotal />
       </div>
     </div>
-  );
+  )
 }
 
-export default Checkout;
+export default Checkout
