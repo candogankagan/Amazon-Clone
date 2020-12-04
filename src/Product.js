f0a5a88c75bb24867f349d19b34bb4ac84274ee2
Kagan Candogan <candogankagan@gmail.com>
2020-12-04 19:34:28 +0300

    remove from basket

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import './Product.css';
-import { useStateValue } from './StateProvider';
+import React from 'react'
+import './Product.css'
+import { useStateValue } from './StateProvider'
 
 function Product({ id, title, image, price, rating }) {
-  const [state, dispatch] = useStateValue();
-  console.log(state);
+  const [state, dispatch] = useStateValue()
   const addToBasket = () => {
     dispatch({
       type: 'ADD_TO_BASKET',
@@ -15,18 +14,18 @@ function Product({ id, title, image, price, rating }) {
         price: price,
         rating: rating,
       },
-    });
-  };
+    })
+  }
 
   return (
-    <div className="product">
-      <div className="product_info">
+    <div className='product'>
+      <div className='product_info'>
         <p>{title}</p>
-        <p className="product_price">
+        <p className='product_price'>
           <small>$</small>
           <strong>{price}</strong>
         </p>
-        <div className="product_rating">
+        <div className='product_rating'>
           {Array(rating)
             .fill()
             .map((_, i) => (
@@ -35,11 +34,11 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
 
-      <img src={image} alt="" />
+      <img src={image} alt='' />
 
       <button onClick={addToBasket}>Add to Basket</button>
     </div>
-  );
+  )
 }
 
-export default Product;
+export default Product
